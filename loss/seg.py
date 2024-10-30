5e6300c8fc4c4912dd69c94018c27dc191f940d8
unknown <am.kazempour@gmail.com>
2024-10-30 16:56:50 +0330

    edit new exp_loss

--- loss/seg.py
+++ loss/seg.py
@@ -19,7 +19,7 @@ def my_Exp_loss(y_true,y_pred):
     return tf.reduce_mean(tf.concat([tf.math.exp(y_pred[y_true==0]),tf.math.exp(-y_pred[y_true==1])],axis=0))
 
 def my_Exp_w_loss(y_true,y_pred):
-    y_p = tf.argmax(y_pred)
+    y_p = tf.argmax(y_pred,axis=-1)
     y_p = tf.keras.utils.to_categorical(y_p,num_classes=y_pred.shape[-1])
     false_pos = tf.math.exp(y_pred[y_p*(1-y_true) == 1])
     false_neg = tf.math.exp(-y_pred[(1-y_p)*y_true == 1])
