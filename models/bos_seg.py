e9e75e5ced2f9f210fc77f63e5b5041e33523508
unknown <am.kazempour@gmail.com>
2024-11-28 16:25:02 +0330

    Add bos_seg file

---
+++ models/bos_seg.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from tensorflow.keras import layers, Model
+
+from .unet import Unet
+
+
+class bos_seg_Unet(Unet):
+
+    def __init__(self, input_shape=..., num_filters=64, class_num=1, batch_norm=True, encoder_num=1,decoder_num=3):
+        self.decoder_num = decoder_num
+        super().__init__(input_shape, num_filters, class_num, batch_norm, encoder_num)
+
+    def _architecture(self):
+        output, c4, c3, c2, c1= self._encoder(self.input)
+        output = self._bottleneck(output)
+        x = []
+        for _ in range(self.decoder_num):
+            x.append(self._head(self._decoder(output, c4, c3, c2, c1)))
+        self.output = layers.concatenate(x)
+    
+    def _head(self,input):
+        x = layers.Conv2D(self.class_num, (1, 1))(input)
+        if self.class_num == 1:
+            output = layers.Activation('sigmoid')(x)
+        else:
+            output = layers.Activation('softmax')(x)
+        
+        return output
