6f36389e477ed520dd4fad87ee3caa24d6adaa8f
unknown <am.kazempour@gmail.com>
2025-06-01 16:21:51 +0330

    Add W_ssim loss (torch)

--- torch/loss/VAE.py
+++ torch/loss/VAE.py
@@ -11,3 +11,29 @@ class ssimloss(nn.Module):
     def forward(self, preds, targets):
         loss = 1 - self.ssim(preds, targets)
         return loss
+
+
+class WeightedSSIMLoss(nn.Module):
+    def __init__(self, alpha=0.8, lesion_weight=2.0, data_range=1.0):
+        super().__init__()
+        self.alpha = alpha
+        self.lesion_weight = lesion_weight
+        self.data_range = data_range
+        self.ssim = StructuralSimilarityIndexMeasure(data_range=1.0).to("cuda")
+
+    def forward(self, y_pred: torch.Tensor, 
+                      y_true: torch.Tensor, 
+                      lesion_mask: torch.Tensor) -> torch.Tensor:
+        # ssim_val = ssim(y_pred, y_true, data_range=self.data_range, size_average=True)
+        ssim_val = self.ssim(y_pred, y_true)
+        ssim_loss = 1.0 - ssim_val
+
+        mae_map = torch.abs(y_true - y_pred)          # [B, C, H, W]
+
+        # mae_map = mae_map.mean(dim=1, keepdim=True)  # [B, 1, H, W]
+
+        weight_map = 1.0 + (self.lesion_weight - 1.0) * lesion_mask
+        weighted_mae = (mae_map * weight_map).mean()
+
+        loss = self.alpha * ssim_loss + (1.0 - self.alpha) * weighted_mae
+        return loss
